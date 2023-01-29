132370d9e90ca5f4531a2da0f25839ffca26527f
Yogesh bhatt <bhattyogesh700@gmail.com>
2023-01-29 03:17:39 +0200

    app sorted

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from utils.db import db
 
-app = Flask(__name__)
+app = Flask(__name__)                                  #user   #password         #database
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/tododb'
 app.config["SECRET_KEY"] = 'thisisthekey'
 
