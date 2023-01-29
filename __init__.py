ff8bb703d916fc675b7fcc287ec133ec2462d45c
SMR <55330312+smr-yogesh@users.noreply.github.com>
2023-01-30 00:17:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from utils.db import db
 
 app = Flask(__name__)                                  #user   #password         #database
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/tododb'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@pgsql:5432/tododb'
 app.config["SECRET_KEY"] = 'thisisthekey'
 
 db.init_app(app)
